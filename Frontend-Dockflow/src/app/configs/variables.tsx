fe956043760ab4ee845f8814bd288e26a48a3014
Josue Zea <jdzeaherrera@gmail.com>
2025-09-22 17:34:24 -0600

    fixing variables

--- Frontend-Dockflow/src/app/configs/variables.tsx
+++ Frontend-Dockflow/src/app/configs/variables.tsx
@@ -1,5 +1,5 @@
 const production = false
 
 export const variables = {
-    host: production ? 'http://131.107.5.84:3000' : 'http://172.206.225.47:3000'
+    host: production ? 'http://131.107.5.84:3000' : 'http://localhost:3000'
 }
